ac8d99a628b2b2ae65b16bc2b084f1ede378cea4
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2023-01-06 15:49:43 +0100

    Solve day 6 part 2

--- src/day06/index.js
+++ src/day06/index.js
@@ -1,30 +1,18 @@
 import run from "aocrunner"
 
-const part1 = (input) => {
+const solve = (input, n) => {
   for(var i = 0; i < input.length; i++) {
-    if (new Set(input.substring(i, i+4).split('')).size == 4) {
-      return i+4
+    if (new Set(input.substring(i, i+n).split('')).size == n) {
+      return i+n
     }
   }
 }
 
-const part2 = (input) => {
-}
-
-const part1Input = `nppdvjthqldpwncqszvftbrmjlhg`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: '' },
-    ],
-    solution: part1,
+    solution: (input) => solve(input, 4),
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: '' },
-    ],
-    solution: part2,
+    solution: (input) => solve(input, 14),
   },
-  onlyTests: false,
 })
