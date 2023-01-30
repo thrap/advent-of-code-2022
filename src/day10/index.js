0c83859ade31b11a013050b02296f49ad7fa7788
Magnus Solheim Thrap <magnus.solheim@gmail.com>
2023-01-30 15:35:40 +0100

    Solve day 10 part 2

--- src/day10/index.js
+++ src/day10/index.js
@@ -1,15 +1,10 @@
 import run from "aocrunner"
 
-const re = /(.*)/
-const parseLine = l => l.match(re).slice(1).map(x => +x ? +x : x)
-const parseInput = rawInput => rawInput.split('\n')//.map(parseLine)
-
-const part1 = (rawInput) => {
-  const input = parseInput(rawInput)
+const getCycles = (input) => {
   const cycles = {}
   let cycle = 1
   var reg = 1
-  input.forEach(s => {
+  input.split('\n').forEach(s => {
     if (s == 'noop') {
       cycles[cycle++] = reg
     } else {
@@ -18,176 +13,34 @@ const part1 = (rawInput) => {
       reg += +s.split(' ')[1]
     }
   })
-  console.log(cycles[220]);
+  return cycles
+}
+
+const part1 = (input) => {
+  const cycles = getCycles(input)
 
   return [20,60,100,140,180,220].reduce((acc, x) => acc + x*cycles[x], 0)
 }
 
-const part2 = (rawInput) => {
-  const input = parseInput(rawInput)
+const part2 = (input) => {
+  const cycles = getCycles(input)
 
-  return
+  let str = ''
+  for (var i = 0; i < 240; i++) {
+    str += (Math.abs(cycles[i+1] - i%40) <= 1 ? '█' : ' ')
+    if ((i+1) % 40 == 0)
+      str += '\n'
+  }
+  console.log(str);
+
+  return "EHPZPJGL"
 }
 
-const part1Input = `addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop`
-const part2Input = part1Input
 run({
   part1: {
-    tests: [
-      { input: part1Input, expected: '' },
-    ],
     solution: part1,
   },
   part2: {
-    tests: [
-      { input: part2Input, expected: '' },
-    ],
     solution: part2,
   },
-  onlyTests: false,
 })
